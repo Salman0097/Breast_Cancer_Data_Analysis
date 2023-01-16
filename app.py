25cd23fa9b644454fa4f44df6045a63e839196af
Salman0097 <ibrahimjrsalman@outlook.com>
2023-01-16 22:03:29 +0530

    adding Procfile

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 app = Flask(__name__)
 svm_model_pkl = pickle.load(open('classifiers.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+piping=pickle.load(open('piping.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -17,7 +17,7 @@ def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=piping.transform(np.array(list(data.values())).reshape(1,-1))
     output=svm_model_pkl.predict(new_data)
     print(output[0])
     return jsonify(output[0])
@@ -25,7 +25,7 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    final_input=piping.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=svm_model_pkl.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
